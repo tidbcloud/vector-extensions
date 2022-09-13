2690d96dab1ecb6eda68cdbcb66661e9906ab51c
Zhenchi <zhongzc_arch@outlook.com>
2022-09-13 17:18:06 +0800

    fix test
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- extensions/vm-import/src/encoder.rs
+++ extensions/vm-import/src/encoder.rs
@@ -149,8 +149,7 @@ mod tests {
                 .unwrap();
             let labels = event.get_mut("labels").unwrap();
             labels
-                .insert("cluster_id", Value::Bytes(Bytes::from("10086")))
-                .unwrap();
+                .insert("cluster_id", Value::Bytes(Bytes::from("10086")));
 
             let value = encoder.encode_event(event.into()).unwrap();
             let (json, key) = value.into_parts();
