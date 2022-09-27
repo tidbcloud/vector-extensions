b7445eda0100552548edea49b84045be086d225f
Zhenchi <zhongzc_arch@outlook.com>
2022-09-27 09:57:47 +0800

    support aarch64
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- scripts/release-docker.sh
+++ scripts/release-docker.sh
@@ -8,7 +8,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # the temp directory used, within $DIR
 # omit the -p parameter to create a temporal directory in the default location
-WORK_DIR=`mktemp -d -p "$DIR"`
+# WORK_DIR=`mktemp -d -p "$DIR"`
+WORK_DIR=work-dir
+mkdir -p "$WORK_DIR"
 
 # check if tmp dir was created
 if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
@@ -25,10 +27,9 @@ function cleanup {
 # register the cleanup function to be called on the EXIT signal
 trap cleanup EXIT
 
-
-## build docker image in the setup directory
-
-cp target/x86_64-unknown-linux-gnu/release/vector "$WORK_DIR"
+# Prepare in the setup directory
+cp target/x86_64-unknown-linux-gnu/release/vector "$WORK_DIR"/vector-amd64
+cp target/aarch64-unknown-linux-gnu/release/vector "$WORK_DIR"/vector-arm64
 cp config/vector.toml "$WORK_DIR"
 
 VERSION="${VECTOR_VERSION:-"$(scripts/version.sh)"}"
@@ -38,5 +39,6 @@ BASE=debian
 TAG="${TAG:-$REPO:$VERSION-$BASE}"
 DOCKERFILE="scripts/docker/Dockerfile"
 
-echo "Building docker image: $TAG"
-docker build -t "$TAG" -f "$DOCKERFILE" "$WORK_DIR"
+PLATFORMS="linux/amd64,linux/arm64"
+echo "Building docker image: $TAG for $PLATFORMS"
+docker buildx build --push --platform="$PLATFORMS" -t "$TAG" -f "$DOCKERFILE" "$WORK_DIR"
