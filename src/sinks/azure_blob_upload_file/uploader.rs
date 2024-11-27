d534d78f1c75e4a2ef3b7d36a396d1002f1fb65d
mornyx <mornyx.z@gmail.com>
2024-11-28 00:41:03 +0800

    fix azure_blob_upload_file: remove o11y_tag due to not supported
    Signed-off-by: mornyx <mornyx.z@gmail.com>

--- src/sinks/azure_blob_upload_file/uploader.rs
+++ src/sinks/azure_blob_upload_file/uploader.rs
@@ -1,11 +1,8 @@
-use std::collections::HashMap;
 use std::io;
 use std::sync::Arc;
 
 use azure_storage_blobs::prelude::*;
-use base64::Engine;
 use bytes::Bytes;
-use md5::{Digest, Md5};
 use tokio::fs::File;
 use tokio::io::AsyncReadExt;
 
@@ -50,20 +47,7 @@ impl AzureBlobUploader {
             .await
         {
             Err(_) => Ok(true),
-            Ok(resp) => match resp.blob.tags {
-                None => Ok(true),
-                Some(tags) => {
-                    let tags: HashMap<String, String> = tags.into();
-                    match tags.get("o11y_etag") {
-                        None => Ok(true),
-                        Some(etag) => {
-                            let file_md5 =
-                                self.calculate_file_md5_hash(&upload_key.filename).await?;
-                            Ok(*etag != file_md5)
-                        }
-                    }
-                }
-            },
+            Ok(_) => Ok(false),
         }
     }
 
@@ -84,15 +68,11 @@ impl AzureBlobUploader {
         file: &mut File,
         file_size: u64,
     ) -> io::Result<usize> {
-        let file_md5 = self.calculate_file_md5_hash(&upload_key.filename).await?;
-        let mut tags = HashMap::new();
-        tags.insert("o11y_etag".to_string(), file_md5);
         let mut buffer = Vec::with_capacity(file_size as usize);
         file.read_to_end(&mut buffer).await?;
         let client = self.client.blob_client(&upload_key.object_key);
         client
             .put_block_blob(buffer)
-            .tags(tags)
             .content_type("application/octet-stream")
             .await
             .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
@@ -105,9 +85,6 @@ impl AzureBlobUploader {
         file: &mut File,
         file_size: u64,
     ) -> io::Result<usize> {
-        let file_md5 = self.calculate_file_md5_hash(&upload_key.filename).await?;
-        let mut tags = HashMap::new();
-        tags.insert("o11y_etag".to_string(), file_md5);
         let client = self.client.blob_client(&upload_key.object_key);
         let mut block_list = Vec::new();
         let mut uploaded_size = 0;
@@ -130,25 +107,9 @@ impl AzureBlobUploader {
         }
         client
             .put_block_list(BlockList { blocks: block_list })
-            .tags(tags)
             .content_type("application/octet-stream")
             .await
             .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
         Ok(uploaded_size as usize)
-    }
-
-    async fn calculate_file_md5_hash(&self, filename: &str) -> io::Result<String> {
-        let mut file = File::open(filename).await?;
-        let mut hasher = Md5::new();
-        let mut buffer = [0; 8096];
-        loop {
-            let n = file.read(&mut buffer).await?;
-            if n == 0 {
-                break;
-            }
-            hasher.update(&buffer[..n]);
-        }
-        let res = hasher.finalize();
-        Ok(base64::prelude::BASE64_STANDARD.encode(&res[..]))
     }
 }
