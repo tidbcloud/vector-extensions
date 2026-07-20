4c68515ee49bef48542097838c16d767eb5590c1
zhou.cai <zhou.cai@pingcap.com>
2026-07-20 11:28:39 +0800

    improve build dockerfile to use latest image

--- scripts/cross/bootstrap-ubuntu.sh
+++ scripts/cross/bootstrap-ubuntu.sh
@@ -3,28 +3,17 @@ set -o errexit
 
 echo 'Acquire::Retries "5";' > /etc/apt/apt.conf.d/80-retries
 
-apt-get update
-apt-get upgrade -y
-
-apt-get install -y \
-  apt-transport-https \
-  gnupg \
-  wget
-
-# we need LLVM >= 3.9 for onig_sys/bindgen
-
-cat <<-EOF > /etc/apt/sources.list.d/llvm.list
-deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main
-deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main
-EOF
-
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key| apt-key add -
+export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get upgrade -y
 
-# needed by onig_sys
+# LLVM >= 3.9 is required by onig_sys/bindgen. Use distro packages from the
+# cross-rs base image (Ubuntu 24.04) instead of the legacy xenial LLVM repo.
 apt-get install -y \
-      libclang1-9 \
-      llvm-9 \
-      unzip
+  clang \
+  curl \
+  libclang-dev \
+  unzip
+
+rm -rf /var/lib/apt/lists/*
